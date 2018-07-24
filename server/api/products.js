ccc1fc859b3f1fbb27310e3c88e0ba565213e3d9
Ali Ebner <ebner.ali@gmail.com>
2018-07-24 15:57:46 -0400

    get route

---
+++ server/api/products.js
@@ -0,0 +1,22 @@
+const router = require('express').Router()
+const { Product } = require('../db/models')
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll({include: [{
+      model: Category
+    }]})
+    if (!products) {
+      const err = new Error('Products not found!')
+      err.status = 404
+      return next(err)
+    }
+    res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+
+
+module.exports = router
