ffcae0ee68773e6b22aeafe86b286beda21e260c
Karalyn Faye <karalyn.ferrari@gmail.com>
2018-07-25 12:17:33 -0400

    product Reducer filter fully functional

--- client/store/productReducer.spec.js
+++ client/store/productReducer.spec.js
@@ -82,7 +82,6 @@ describe('thunk creators', () => {
       mockAxios.onGet('/api/products/').replyOnce(200, fakeProducts)
       await store.dispatch(fetchProducts())
       const actions = store.getActions()
-      console.log(actions)
       expect(actions[0].type).to.be.equal(SET_PRODUCTS)
       expect(actions[0].productList).to.be.deep.equal(fakeProducts)
     })
@@ -94,32 +93,32 @@ describe('thunk creators', () => {
       expect(action).to.deep.equal({type: FILTER_PRODUCTS, filter: 'Family Vacation' })
     })
      const newState = reducer(initialState, action)
-  //   it('filters students by category', () => {
-  //     expect(newState.list).to.deep.equal([{
-  //       "id": 2,
-  //       "title": "Italy",
-  //       "description": "Lets eat some pasta!",
-  //       "price": "1500.00",
-  //       "quantity": 2,
-  //       "photo": "http://covermyfb.com/media/covers/9151-beach.jpg",
-  //       "availability": "Available",
-  //       "createdAt": "2018-07-24T21:30:17.769Z",
-  //       "updatedAt": "2018-07-24T21:30:17.769Z",
-  //       "categories": [
-  //           {
-  //               "id": 2,
-  //               "name": "Family Vacation",
-  //               "createdAt": "2018-07-24T21:30:17.781Z",
-  //               "updatedAt": "2018-07-24T21:30:17.781Z",
-  //               "product_categories": {
-  //                   "createdAt": "2018-07-24T21:30:17.803Z",
-  //                   "updatedAt": "2018-07-24T21:30:17.803Z",
-  //                   "productId": 2,
-  //                   "categoryId": 2
-  //               }
-  //           }
-  //       ]
-  //   }])
-  //   })
+    it('filters students by category', () => {
+      expect(newState.list).to.deep.equal([{
+        "id": 2,
+        "title": "Italy",
+        "description": "Lets eat some pasta!",
+        "price": "1500.00",
+        "quantity": 2,
+        "photo": "http://covermyfb.com/media/covers/9151-beach.jpg",
+        "availability": "Available",
+        "createdAt": "2018-07-24T21:30:17.769Z",
+        "updatedAt": "2018-07-24T21:30:17.769Z",
+        "categories": [
+            {
+                "id": 2,
+                "name": "Family Vacation",
+                "createdAt": "2018-07-24T21:30:17.781Z",
+                "updatedAt": "2018-07-24T21:30:17.781Z",
+                "product_categories": {
+                    "createdAt": "2018-07-24T21:30:17.803Z",
+                    "updatedAt": "2018-07-24T21:30:17.803Z",
+                    "productId": 2,
+                    "categoryId": 2
+                }
+            }
+        ]
+    }])
+    })
    })
 })
