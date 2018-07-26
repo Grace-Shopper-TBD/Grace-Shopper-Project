15161c0a9112240c3da3f27aac91bce0369e9bb7
Shoshana Rosenfield <Shoshana@MacBook-Air-2.local>
2018-07-26 16:12:15 -0400

    updated to include reviews reducer and thunk

--- client/store/index.js
+++ client/store/index.js
@@ -5,8 +5,9 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import product from './productReducer'
 import category from './categoryReducer'
+import review from './reviewReducer'
 
-const reducer = combineReducers({user, product, category})
+const reducer = combineReducers({user, product, category, review})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -15,3 +16,4 @@ const store = createStore(reducer, middleware)
 export default store
 export * from './user'
 export * from './productReducer'
+export * from './reviewReducer'
---
+++ client/store/reviewReducer.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+
+/*
+ * ACTION TYPES
+ */
+export const SET_REVIEWS = 'SET_REVIEWS'
+const LOADING_REVIEWS = 'LOADING_REVIEWS'
+const LOADING_PROBLEM = 'LOADING_PROBLEM'
+/**
+ * INITIAL STATE
+ */
+const reviews = {
+  list: [],
+  isLoading: false,
+  gotError: false
+}
+
+/**
+ * ACTION CREATORS
+ */
+export const setReviews = (reviewList => ({
+  type: SET_REVIEWS,
+  reviewList
+}))
+
+
+/**
+ * THUNK CREATORS
+ */
+
+export const fetchReviews = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/reviews/')
+    dispatch(setReviews(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = reviews, action) {
+  switch (action.type) {
+    case SET_REVIEWS:
+      return {...state, list: action.reviewList, isLoading: false, gotError: false}
+    case LOADING_REVIEWS:
+      return {...state, isLoading: true}
+    case LOADING_PROBLEM:
+      return {...state, gotError: true}
+    default:
+      return state
+  }
+}
