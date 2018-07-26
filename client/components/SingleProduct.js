f38b63e0ce4501fa14a138a670c7863a66e59861
Bronwyn Harris <bronwyn.harris@Bronwyns-MacBook-Air.local>
2018-07-26 11:28:48 -0400

    single product page works now

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -3,20 +3,24 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 // import potential thunks from the store
 
-const SingleProduct = ({product}) => {
-    console.log('is SingleProduct even showing up?', product)
-
+class SingleProduct extends Component {
+    
+    render(){
+        console.log('product going into SingleProduct',this.props.product)
         return(
             <div>
-                <h1>{product.title}</h1>
+                <h1>hello</h1>
             </div>
         )
-    
+    }
 }
 
-const mapState = (state, {match}) => ({
-    product: state.products.find(product => product.id === match.params.id)
-})
+const mapState = (state, {match}) => {
+    console.log('this is the match', match)
+    return {
+        product: state.product.list.find(product => product.id === match.params.id)
+    }
+}
 
 // there is no mapDispatch for now but can be added later for imported thunks
 
