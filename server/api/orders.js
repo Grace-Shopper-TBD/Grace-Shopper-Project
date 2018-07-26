779b8231225245e39dc7c567e5acc76753cf8d12
Ali Ebner <ebner.ali@gmail.com>
2018-07-26 16:20:31 -0400

    order routes including put, post, and delete

--- server/api/orders.js
+++ server/api/orders.js
@@ -25,7 +25,7 @@ router.post('/', async (req, res, next) => {
 		const product = await Product.findById(req.body.productId)
 		req.session.cart = req.session.cart ? await Order.findById(req.session.cart.id)
 		 : await Order.create({
-				userId: 1,
+				userId: req.user.id,
 				isCart: true
 			})
 		const newLineItem = await LineItem.create({
@@ -40,5 +40,74 @@ router.post('/', async (req, res, next) => {
 		next(err)
 	}
 })
+
+router.get('/cart', async (req, res, next) => {
+	try {
+		if (!req.session.cart) {
+			return res.send('Cart is Empty')
+		}
+		const cart = await Order.findById(req.session.cart.id, {
+			include: [{
+				model: Product
+			}]
+		})
+		if (!cart) {
+			const err = new Error('Cart Not Found')
+			return next(err)
+		}
+		else {
+			res.json(cart)
+		}
+	} catch (err) {
+		next(err)
+	}
+})
+
+router.put('/cart/:productId', async (req, res, next) => {
+	try {
+		const lineItem = await LineItem.findOne({
+			where: {
+				orderId: req.session.cart.id,
+				productId: req.params.productId
+			}
+		})
+		const updatedLineItem = await lineItem.update({
+			quantity: req.body.quantity
+		})
+		if (updatedLineItem.quantity === 0) {
+			await updatedLineItem.destroy()
+		}
+		res.json(updatedLineItem)
+	} catch (err) {
+		next(err)
+	}
+})
+
+router.put('/:orderId', authorize, async (req, res, next) => {
+	try {
+		const orderToUpdate = await Order.findById(req.params.orderId)
+		const updatedOrder = await orderToUpdate.update(req.body)
+		res.json(updatedOrder)
+	} catch (err) {
+		next(err)
+	}
+})
+
+router.delete('/cart/:productId', async (req, res, next) => {
+	try {
+		const lineItemToDelete = await LineItem.findOne({
+			where: {
+				orderId: req.session.cart.id,
+				productId: req.params.productId
+			}
+		})
+		await lineItemToDelete.destroy()
+		res.sendStatus(204)
+	} catch (err) {
+		next(err)
+	}
+})
+
+
 
 module.exports = router
