2b63750f0b9ebae9d610f5e99cc8aa6236e52195
Bronwyn Harris <bronwyn.harris@Bronwyns-Air.home>
2018-07-29 21:57:46 -0400

    added dollar sign and how many left

--- client/components/ProductItem.js
+++ client/components/ProductItem.js
@@ -14,8 +14,9 @@ const ProductItem = ({product}) => {
                 <img src={product.photo} className="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap"/>
                         <div className='card-text'>
                             <p>{product.title}</p>
-                            <p>{product.price}</p>
+                            <p>${product.price}</p>
                             <p>{product.availability}</p>
+                            <h3>{product.quantity} Left</h3>
                             <div className="d-flex justify-content-between align-items-center">
                                 <div className="btn-group">
                                     <AddToCart product={product} />                                                    
